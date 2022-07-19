3c2ac8f3fb59b9825d733db7132c0416a5b72086
david kantor <demkantor@gmail.com>
2022-07-19 17:45:42 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import funyun from './assets/single.png';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>State of the Funyun</h1>
       </header>
-    </div>
+      <main>
+        <img src={funyun} alt={'the funyun'} />
+      </main>
+      <footer>
+        <p>fjorge</p>
+      </footer>
+    </>
   );
 }
 
